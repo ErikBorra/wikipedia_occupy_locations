bdfb7e407a3ac1a4fe8f1f98864433a4229744b2
Erik Borra <erik@digitalmethods.net>
2015-08-27 13:11:35 +0200

    first implementation of scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,46 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki # used for running the scraper and storing data
+import requests # used for retrieving data
+from bs4 import BeautifulSoup # used for parsing data
+import re # used for regular expressions
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# store results in structured format
+def add_location(continent, country, city, db):
+    db.append({
+        'continent': continent,
+        'country': country,
+        'city': city
+    })
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# parse table for locations
+def scan_table(table, continent, db):
+    rows = table.find_all('tr') # find all rows in the table
+    hdr = rows.pop(0).find_all('th') # find column headings
+    assert hdr[0].text == u'Country or region:' # verify that first column contains the country or region
+    assert hdr[1].text == u'City or district:' # verify that the second column contains the city or district
+    for row in rows: # loop over all rows
+        td = row.find_all('td') # get each cell
+        country = td[0].text
+        city = td[1].text
+        add_location(continent, country, city, db)
+
+# verify that only tables within a section are retrieved
+def contains_headline(tag):
+    return tag.find(class_='mw-headline') is not None
+
+# main loop 
+# initializes the data structure
+# retrieves the page 
+# locates the tables with locations
+# saves parsed data
+def main():
+    db = [] # initialize data structure
+    soup = BeautifulSoup(requests.get('https://en.wikipedia.org/wiki/List_of_Occupy_movement_protest_locations').text, 'xml') # request wiki page
+    for th in soup.find_all('th', text=re.compile("Country")): # find column headings which contain 'country'
+        table = th.find_parent('table') # locte table in which this column heading is found
+        continent = table.find_previous_sibling(contains_headline).find(class_='mw-headline').findAll(text=True) # get section title, i.e. continent
+        scan_table(table, continent, db)
+        
+    # Write out to the sqlite database using scraperwiki library
+    scraperwiki.sqlite.save(unique_keys=['city'], data=db)
+
+main()
